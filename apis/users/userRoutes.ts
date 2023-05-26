e12045aaef71f1899b82f977d8c84fa3146ab1f9
Cito2310 <cito2310@gmail.com>
2023-05-26 06:06:21 -0300

    implement login && delete route register

--- apis/users/userRoutes.ts
+++ apis/users/userRoutes.ts
@@ -10,38 +10,38 @@ import { createUser, loginUser } from './userControllers';
 export const routeUser = Router();
 
 
-routeUser.post("/register",[
-    check("password").trim()
-        .notEmpty().withMessage("password is required")
-        .isString().withMessage("password not is string")
-        .isLength({min:8, max: 32}).withMessage("password length can only be greater than 8 and less than 24 characters")
-    ,
-
-    check("username").trim()
-        .notEmpty().withMessage("password is required")
-        .isString().withMessage("username not is string")
-        .isLength({min:8, max: 32}).withMessage("username length can only be greater than 8 and less than 24 characters")
-        .custom(checkValidationUser.existWithUsername).withMessage("username it already exists")
-    ,
-
-    checkFields
-], createUser);
-
-
-
-
-// routeUser.post("/login",[
+// routeUser.post("/register",[
 //     check("password").trim()
 //         .notEmpty().withMessage("password is required")
 //         .isString().withMessage("password not is string")
-//         .custom(checkValidationUser.passwordEqual).withMessage("invalid login")
+//         .isLength({min:8, max: 32}).withMessage("password length can only be greater than 8 and less than 24 characters")
 //     ,
 
 //     check("username").trim()
-//         .notEmpty().withMessage("username is required")
+//         .notEmpty().withMessage("password is required")
 //         .isString().withMessage("username not is string")
-//         .custom(checkValidationUser.notExistWithUsername).withMessage("invalid login")
+//         .isLength({min:8, max: 32}).withMessage("username length can only be greater than 8 and less than 24 characters")
+//         .custom(checkValidationUser.existWithUsername).withMessage("username it already exists")
 //     ,
 
 //     checkFields
-// ], loginUser);
+// ], createUser);
+
+
+
+
+routeUser.post("/login",[
+    check("password").trim()
+        .notEmpty().withMessage("password is required")
+        .isString().withMessage("password not is string")
+        .custom(checkValidationUser.passwordEqual).withMessage("invalid login")
+    ,
+
+    check("username").trim()
+        .notEmpty().withMessage("username is required")
+        .isString().withMessage("username not is string")
+        .custom(checkValidationUser.notExistWithUsername).withMessage("invalid login")
+    ,
+
+    checkFields
+], loginUser);
